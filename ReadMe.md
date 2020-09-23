1628f8655d8221c414b1b67bc5fe17e02e70981e
Michael LoPresti <mike@mdlopresti.dev>
2020-09-23 18:10:27 -0400

    add readme

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# TBD Name
+A discord notification bot/learn docker/kubernetes project.
+## Goals
+1. Automatically send out notifications to discord server channels
+2. Use Docker for the runtime enviroment
+3. Use Kubernetes cronjobs to schedule the notifications
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     discord_request = requests.post(webhook, data = payload)
 
     if discord_request.status_code == 204:
-        #print('Successfully sent')
+        print('Successfully sent')
     else:
         print(discord_request.status_code)
         print(discord_request.text)
