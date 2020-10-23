91f5f5eb9af61f1082a517b085567326fb277cd0
Michael LoPresti <mike@mdlopresti.dev>
2020-10-23 15:28:04 -0400

    Added error handling for missing enviroment variables

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import os
 import requests
 
 def main():
-    webhook = os.environ['WEBHOOK']
-    message = os.environ['MESSAGE']
-    username = os.environ['USERNAME']
+    if 'WEBHOOK' in os.environ:
+        if 'MESSAGE' in os.environ:
+            webhook = os.environ['WEBHOOK']
+            message = os.environ['MESSAGE']
+            username = os.environ.get('USERNAME', 'Notification')
 
-    payload = {
-        'content': message,
-        'username': username
-    }
-    discord_request = requests.post(webhook, data = payload)
+            payload = {
+                'content': message,
+                'username': username
+            }
+            discord_request = requests.post(webhook, data = payload)
 
-    if discord_request.status_code == 204:
-        print('Successfully sent')
+            if discord_request.status_code == 204:
+                print('Successfully sent')
+            else:
+                print(discord_request.status_code)
+                print(discord_request.text)
+        else:
+            print('MESSAGE is a mandatory environment variable and is not set')
     else:
-        print(discord_request.status_code)
-        print(discord_request.text)
-
+        print('WEBHOOK is a mandatory environment variable and is not set')
+    
 main()
