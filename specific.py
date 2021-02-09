c91c3a11bad8618595bba286e7460b5efa15c6ea
Michael LoPresti <mike@lopresti.org>
2021-02-09 12:25:18 -0500

    Adding better comments

--- specific.py
+++ specific.py
@@ -8,13 +8,14 @@ def message():
         print('NEXT_DATE is ' + os.environ['NEXT_DATE'])
         if 'NEXT_TIME' in os.environ:
             print('NEXT_TIME is ' + os.environ['NEXT_TIME'])
-            #next_date_list = '2021','2','17'
+            #parsing date from enviroment variable
             next_date_list = os.environ['NEXT_DATE'].split()
-
             next_date = datetime.datetime.strptime(next_date_list[0] + ' ' + next_date_list[1] + ' ' + next_date_list[2], '%Y %m %d').date()
-            today = datetime.datetime.now(tz=timezone('US/Eastern')).date()
-            days_until_session = (next_date - today).days
+            
+            # calculating the days until the next session
+            days_until_session = (next_date - datetime.datetime.now(tz=timezone('US/Eastern')).date()).days
 
+            #building message for alert
             if days_until_session == 7:
                 os.environ['MESSAGE'] = 'One week until the next session!'
             elif days_until_session == 0:
@@ -23,8 +24,7 @@ def message():
                 os.environ['MESSAGE'] = str(days_until_session) + ' days until the next session, which will be on ' + next_date.strftime('%A %B %d, %Y') + ' at ' + os.environ['NEXT_TIME']
 
             print('MESSAGE will be "' + os.environ['MESSAGE'] + '"')
-            os.system('python main.py')
-
+            #calling main function from other python script
             main()
 
         else:
