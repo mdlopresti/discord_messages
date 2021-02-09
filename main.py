aa330261c60d11cc1158336585cfce085838b0fd
Michael LoPresti <mike@lopresti.org>
2021-02-09 12:28:03 -0500

    adding comments to code

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import requests
 def main():
     if 'WEBHOOK' in os.environ:
         if 'MESSAGE' in os.environ:
+            #parsing environment variables
             webhook = os.environ['WEBHOOK']
             message = os.environ['MESSAGE']
             username = os.environ.get('USERNAME', 'Notification')
 
+            #building payload for discord service
             payload = {
                 'content': message,
                 'username': username
             }
+            
+            #calling discord webhook
             discord_request = requests.post(webhook, data = payload)
 
             if discord_request.status_code == 204:
